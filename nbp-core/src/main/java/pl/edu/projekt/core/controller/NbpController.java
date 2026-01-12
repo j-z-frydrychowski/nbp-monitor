93bb139b69c9d48ea3ccaab1fe18a99ea58da9d9
jzfrydrychowski <s32713@pjwstk.edu.pl>
2026-01-12 23:23:10 +0100

    Add NbpController

---
+++ nbp-core/src/main/java/pl/edu/projekt/core/controller/NbpController.java
@@ -0,0 +1,28 @@
+package pl.edu.projekt.core.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.edu.projekt.core.service.NbpService;
+
+@RestController
+@RequestMapping("/nbp")
+public class NbpController {
+
+    private final NbpService nbpService;
+
+    public NbpController(NbpService nbpService) {
+        this.nbpService = nbpService;
+    }
+
+    /**
+     * Endpoint do ręcznego wymuszenia pobrania danych.
+     * Użycie: GET http://localhost:8080/nbp/load/A
+     */
+    @GetMapping("/load/{table}")
+    public String loadRates(@PathVariable String table) {
+        nbpService.fetchAndSaveRates();
+        return "Pomyślnie pobrano i zapisano dane dla tabeli: ";
+    }
+}
